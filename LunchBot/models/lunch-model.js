c706af46d7c9223f9da518072ea57ec39cbd0396
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-04 17:24:39 -0700

    added model

---
+++ LunchBot/models/lunch-model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LunchSchema = new Schema({
+  lunchLocation: String,
+  owner: [UserSchema],
+  attendees: [[UserSchema]],
+  time: Date,
+  open: {type: Boolean, default: true},
+  timestampCreated: {type: Date, default: Date.now()}
+
+});
+
+// Create user schema
+const UserSchema = new Schema({
+  id: String,
+
+});
+
+// Create user model (which will house each person's list)
+const Lunch = mongoose.model('lunch', LunchSchema);
